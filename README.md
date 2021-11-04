4550525e62b05b01015da28d61dc0bf59196fa3f
Martin Vysny <mavi@vaadin.com>
2021-11-04 11:02:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Karibu-Migration: Aids migration from Vaadin 8 to Vaadin 14+
+
+[![GitHub tag](https://img.shields.io/github/tag/mvysny/karibu-migration.svg)](https://github.com/mvysny/karibu-migration/tags)
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.github.mvysny.karibu-migration/karibu-migration/badge.svg)](https://maven-badges.herokuapp.com/maven-central/com.github.mvysny.karibu-migration/karibu-migration)
+
+A collection of extension functions mimicking Vaadin 8 API but redirecting to Vaadin 14+ API where applicable. For example,
+there is `setWidthUndefined()` function marked as deprecated (since it's no longer in Vaadin 14), redirecting to a `setWidth(null)` call.
+
+The jar is in Maven Central (TODO release), so it's easy to add this library to your project.
+
+Gradle:
+```groovy
+repositories {
+  mavenCentral()
+}
+dependencies {
+  api("com.github.mvysny.karibu-migration:karibu-migration:x.y")
+}
+```
+
+See the tag above for the latest version.
+
+# List of functions
+
+TBA
+
+# License
+
+Licensed under [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0.html)
+
+Copyright 2021-2022 Martin Vysny
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this software except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+# Contributing / Developing
+
+See [Contributing](CONTRIBUTING.md).
---
+++ build.gradle.kts
@@ -0,0 +1,122 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    `maven-publish`
+    signing
+}
+
+defaultTasks("clean", "build")
+
+allprojects {
+    group = "com.github.mvysny.karibu-migration"
+    version = "0.1-SNAPSHOT"
+
+    apply {
+        plugin("kotlin")
+    }
+
+    repositories {
+        mavenCentral()
+        maven(url = "https://maven.vaadin.com/vaadin-prereleases/")
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            // to see the exceptions of failed tests in Travis-CI console.
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+    }
+}
+
+kotlin {
+    explicitApi()
+}
+
+dependencies {
+    api(kotlin("stdlib-jdk8"))
+    // Vaadin 14
+    compileOnly("com.vaadin:vaadin-core:${properties["vaadin14_version"]}") {
+        // Webjars are only needed when running in Vaadin 13 compatibility mode
+        listOf("com.vaadin.webjar", "org.webjars.bowergithub.insites",
+                "org.webjars.bowergithub.polymer", "org.webjars.bowergithub.polymerelements",
+                "org.webjars.bowergithub.vaadin", "org.webjars.bowergithub.webcomponents")
+                .forEach { exclude(group = it) }
+    }
+    compileOnly("javax.servlet:javax.servlet-api:3.1.0")
+    // IDEA language injections
+    api("org.jetbrains:annotations:22.0.0")
+
+    testImplementation("com.vaadin:vaadin-core:${properties["vaadin14_version"]}") {
+        // Webjars are only needed when running in Vaadin 13 compatibility mode
+        listOf("com.vaadin.webjar", "org.webjars.bowergithub.insites",
+                "org.webjars.bowergithub.polymer", "org.webjars.bowergithub.polymerelements",
+                "org.webjars.bowergithub.vaadin", "org.webjars.bowergithub.webcomponents")
+                .forEach { exclude(group = it) }
+    }
+    testImplementation("com.github.mvysny.dynatest:dynatest:${properties["dynatest_version"]}")
+    testImplementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
+}
+
+// following https://dev.to/kengotoda/deploying-to-ossrh-with-gradle-in-2020-1lhi
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.withType<Javadoc> {
+    isFailOnError = false
+}
+
+publishing {
+    repositories {
+        maven {
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.properties["ossrhUsername"] as String? ?: "Unknown user"
+                password = project.properties["ossrhPassword"] as String? ?: "Unknown user"
+            }
+        }
+    }
+    publications {
+        create("mavenJava", MavenPublication::class.java).apply {
+            groupId = project.group.toString()
+            this.artifactId = "karibu-migration"
+            version = project.version.toString()
+            pom {
+                description.set("Karibu-Migration: Aids migration from Vaadin 8 to Vaadin 14+")
+                name.set("Karibu-Migration")
+                url.set("https://github.com/mvysny/karibu-migration")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mavi")
+                        name.set("Martin Vysny")
+                        email.set("martin@vysny.me")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/mvysny/karibu-migration")
+                }
+            }
+            from(components["java"])
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
