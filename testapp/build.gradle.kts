0ade6d2589c89d8c1b5c35bfaf88f12d15f0a593
Martin Vysny <mavi@vaadin.com>
2024-04-11 19:35:06 +0300

    move deps to toml

--- testapp/build.gradle.kts
+++ testapp/build.gradle.kts
@@ -15,17 +15,17 @@ gretty {
 dependencies {
     implementation(project(":karibu-migration-kotlin"))
     // Vaadin 14
-    implementation("com.vaadin:vaadin-core:${properties["vaadin14_version"]}") {
+    implementation(libs.vaadin.v14.core) {
         // Webjars are only needed when running in Vaadin 13 compatibility mode
         listOf("com.vaadin.webjar", "org.webjars.bowergithub.insites",
                 "org.webjars.bowergithub.polymer", "org.webjars.bowergithub.polymerelements",
                 "org.webjars.bowergithub.vaadin", "org.webjars.bowergithub.webcomponents")
                 .forEach { exclude(group = it) }
     }
-    providedCompile("javax.servlet:javax.servlet-api:3.1.0")
-    implementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
+    providedCompile(libs.javax.servletapi)
+    implementation(libs.slf4j.simple)
 
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:${properties["kaributesting_version"]}")
-    testImplementation("com.github.mvysny.dynatest:dynatest:${properties["dynatest_version"]}")
+    testImplementation(libs.karibu.testing)
+    testImplementation(libs.dynatest)
 }
 
